9aaa51aaccce6a18ccd1e15b6a44ed50dcada169
star.yar <vestnik.yar@gmail.com>
2022-11-08 12:09:26 -0500

    Add two samplers

---
+++ src/btech_experiment/sessions.py
@@ -0,0 +1,73 @@
+import typing as tp
+
+import numpy as np
+import pandas as pd
+
+from btech_experiment.historical_data_sampler import HistoricBasedSampleParams, StratifiedGroups
+
+
+def sample_sessions_from_user_sessions(
+    df_user_sessions: pd.DataFrame,
+    n_unique_users_for_period: pd.Series,
+    sample_params: HistoricBasedSampleParams,
+) -> StratifiedGroups:
+    sample_pilot = {}
+    sample_control = {}
+    sample_sizes = sample_params.get_sample_size(n_unique_users_for_period)
+    for strata_name, strata_data in df_user_sessions.groupby('platform'):
+        strata_sample_size = sample_sizes[strata_name]
+        general_sample = (
+            strata_data[['sessions', 'conversions']]
+            .sample(strata_sample_size)
+        )
+        groups_sizes = sample_params.get_groups_sizes(strata_sample_size)
+        sample_pilot[strata_name] = general_sample.head(groups_sizes.pilot)
+        sample_control[strata_name] = general_sample.tail(groups_sizes.control)
+    return StratifiedGroups(pilot=pd.Series(sample_pilot), control=pd.Series(sample_control))
+
+
+def stratified_metric_estimator_for_sessions(
+    group: tp.Mapping[str, tp.Union[np.ndarray, pd.DataFrame]],
+    strats_weights: pd.Series,
+) -> float:
+    group_mean = {
+        strata_name: (
+            strata_data['conversions'].sum() / strata_data['sessions'].sum()
+            if isinstance(strata_data, pd.DataFrame)
+            else strata_data.sum(axis=0)[:, 0] / strata_data.sum(axis=0)[:, 1]
+        )
+        for strata_name, strata_data in group.items()
+    }
+    return sum(
+        group_weight * group_mean[group_id]
+        for group_id, group_weight in strats_weights.items()
+    )
+
+
+def bootstrap_strata_conversions(
+    strat_data: pd.DataFrame, bootstrap_size: int,
+) -> np.array:
+    sample_size = strat_data.shape[0]
+    sampled_strat = strat_data.sample(
+        bootstrap_size * sample_size, replace=True,
+    )
+    assert len(strat_data.columns) == 2
+    return (
+        sampled_strat[['conversions', 'sessions']]
+        .values.reshape(sample_size, bootstrap_size, -1)
+    )
+
+
+def stratified_sample_bootstrapper_for_sessions(
+    boostrap_size: int, groups: StratifiedGroups,
+) -> StratifiedGroups:
+    return StratifiedGroups(
+        control={
+            name: bootstrap_strata_conversions(data, boostrap_size)
+            for name, data in groups.control.items()
+        },
+        pilot={
+            name: bootstrap_strata_conversions(data, boostrap_size)
+            for name, data in groups.pilot.items()
+        },
+    )
---
+++ src/btech_experiment/users_conversions.py
@@ -0,0 +1,56 @@
+import numpy as np
+import pandas as pd
+
+from btech_experiment.historical_data_sampler import HistoricBasedSampleParams, StratifiedGroups
+
+
+def sample_user_conversions_from_user_sessions(
+    df_user_sessions: pd.DataFrame,
+    n_unique_users_for_period: pd.Series,
+    sample_params: HistoricBasedSampleParams,
+) -> StratifiedGroups:
+    sample_pilot = {}
+    sample_control = {}
+    sample_sizes = sample_params.get_sample_size(
+        n_unique_users_for_period
+    )
+    for strata_name, strata_data in df_user_sessions.groupby('platform'):
+        strata_sample_size = sample_sizes[strata_name]
+        general_sample = (
+            strata_data['conversion_proba']
+            .sample(strata_sample_size)
+        )
+        groups_sizes = sample_params.get_groups_sizes(strata_sample_size)
+        sample_pilot[strata_name] = general_sample.head(
+            groups_sizes.pilot
+        ).values
+        sample_control[strata_name] = general_sample.tail(
+            groups_sizes.control
+        ).values
+    return StratifiedGroups(
+        pilot=pd.Series(sample_pilot),
+        control=pd.Series(sample_control),
+    )
+
+
+def stratified_sample_bootstrapper_for_users(
+    boostrap_size: int, groups: StratifiedGroups,
+) -> StratifiedGroups:
+    return StratifiedGroups(
+        groups.control.apply(
+            lambda x: np.random.choice(x, (x.size, boostrap_size))
+        ),
+        groups.pilot.apply(
+            lambda x: np.random.choice(x, (x.size, boostrap_size))
+        ),
+    )
+
+
+def stratified_metric_estimator_for_users(
+    group: pd.Series, strats_weights: pd.Series,
+) -> float:
+    group_mean = group.apply(np.mean, axis=0)
+    return sum(
+        group_weight * group_mean[group_id]
+        for group_id, group_weight in strats_weights.items()
+    )
