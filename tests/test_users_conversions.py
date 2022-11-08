1bf983560b7e73defc1cef76362842eb34a57771
star.yar <vestnik.yar@gmail.com>
2022-11-08 15:30:52 -0500

    Refactor test

--- tests/test_users_conversions.py
+++ tests/test_users_conversions.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import pytest
 
 from btech_experiment.data_sampler import (
     HistoricBasedSampleParams,
@@ -9,44 +10,79 @@ from btech_experiment.data_sampler import (
 from duration_estimator import Effect
 
 
-def test_sampling_sessions(
-        df_daily_users: pd.DataFrame,
-        df_user_sessions: pd.DataFrame,
-) -> None:
-    # test sampler
-    sampler = HistoricalUsersConversionsSampler(df_daily_users, df_user_sessions)
-    groups = sampler(n_days=1, sample_params=HistoricBasedSampleParams(0.15, 0.2))
-    assert 'ANDROID' in groups.pilot.keys()
-    assert 'IOS' in groups.pilot.keys()
-    assert groups.pilot['ANDROID'].shape == (2073,)
-    assert groups.pilot['IOS'].shape == (418,)
+class TestUserSessionsCase:
+    @pytest.fixture
+    def strats_weights(self, df_daily_users: pd.DataFrame) -> pd.Series:
+        return eval_strats_weights(df_daily_users)
 
-    # test metric on initial sample
-    st_wt = eval_strats_weights(df_daily_users)
-    metrics = SessionsBootstrap.estimate_metric(groups, strats_weights=st_wt)
-    assert isinstance(metrics.pilot, float)
-    assert isinstance(metrics.control, float)
+    @pytest.fixture
+    def sampler(
+            self,
+            df_daily_users: pd.DataFrame,
+            df_user_sessions: pd.DataFrame,
+    ) -> HistoricalUsersConversionsSampler:
+        return HistoricalUsersConversionsSampler(df_daily_users, df_user_sessions)
 
-    # test bootstrapping
-    bootstrapped_samples = SessionsBootstrap.bootstrap_sample(5, groups)
-    assert 'ANDROID' in groups.pilot.keys()
-    assert 'IOS' in groups.pilot.keys()
-    assert (
-            bootstrapped_samples.pilot['ANDROID'].shape
-            == (bootstrapped_samples.pilot['ANDROID'].shape[0], 5)
-    )
-    assert (
-            bootstrapped_samples.pilot['IOS'].shape
-            == (bootstrapped_samples.pilot['IOS'].shape[0], 5)
-    )
+    def test_sampling(
+            self, sampler: HistoricalUsersConversionsSampler,
+    ) -> None:
+        groups = sampler(n_days=1, sample_params=HistoricBasedSampleParams(0.15, 0.2))
+        assert 'ANDROID' in groups.pilot.keys()
+        assert 'IOS' in groups.pilot.keys()
+        assert groups.pilot['ANDROID'].shape == (2073,)
+        assert groups.pilot['IOS'].shape == (418,)
 
-    # test metric for boot sample
-    boot_metric = SessionsBootstrap.estimate_metric(
-        bootstrapped_samples, strats_weights=st_wt,
-    )
-    assert len(boot_metric.pilot) == 5
+    def test_evaluating_metric_on_initial_sample(
+            self,
+            sampler: HistoricalUsersConversionsSampler,
+            strats_weights: pd.Series,
+    ) -> None:
+        groups = sampler(n_days=1, sample_params=HistoricBasedSampleParams(0.15, 0.2))
 
-    # try end-to_end
-    found_effect = SessionsBootstrap(st_wt)(groups, Effect(0.5, is_additive=False))
-    assert found_effect.given_effect
-    assert not found_effect.given_no_effect
+        # test metric on initial sample
+        metrics = SessionsBootstrap.estimate_metric(groups, strats_weights=strats_weights)
+        assert isinstance(metrics.pilot, float)
+        assert isinstance(metrics.control, float)
+
+    def test_bootstrapping_sample(
+            self,
+            sampler: HistoricalUsersConversionsSampler,
+    ) -> None:
+        groups = sampler(n_days=1, sample_params=HistoricBasedSampleParams(0.15, 0.2))
+
+        # test bootstrapping
+        bootstrapped_samples = SessionsBootstrap.bootstrap_sample(5, groups)
+        assert 'ANDROID' in groups.pilot.keys()
+        assert 'IOS' in groups.pilot.keys()
+        assert (
+                bootstrapped_samples.pilot['ANDROID'].shape
+                == (bootstrapped_samples.pilot['ANDROID'].shape[0], 5)
+        )
+        assert (
+                bootstrapped_samples.pilot['IOS'].shape
+                == (bootstrapped_samples.pilot['IOS'].shape[0], 5)
+        )
+
+    def test_evaluating_metric_on_bootstrapped_sample(
+            self,
+            sampler: HistoricalUsersConversionsSampler,
+            strats_weights: pd.Series,
+    ) -> None:
+        groups = sampler(n_days=1, sample_params=HistoricBasedSampleParams(0.15, 0.2))
+        bootstrapped_samples = SessionsBootstrap.bootstrap_sample(5, groups)
+
+        boot_metric = SessionsBootstrap.estimate_metric(
+            bootstrapped_samples, strats_weights=strats_weights,
+        )
+        assert len(boot_metric.pilot) == 5
+
+    def test_end_to_end(
+            self,
+            sampler: HistoricalUsersConversionsSampler,
+            strats_weights: pd.Series,
+    ) -> None:
+        groups = sampler(n_days=1, sample_params=HistoricBasedSampleParams(0.15, 0.2))
+
+        found_effect = SessionsBootstrap(strats_weights)(groups, Effect(0.5, is_additive=False))
+        assert found_effect.given_effect
+        assert not found_effect.given_no_effect
