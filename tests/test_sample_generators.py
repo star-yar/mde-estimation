bac474f26d187ae7af0260bfceba613632fa9647
star-yar <vestnik.yar@gmail.com>
2024-06-10 17:16:23 -0400

    Fix normal dist test

--- tests/test_sample_generators.py
+++ tests/test_sample_generators.py
@@ -6,9 +6,9 @@ from duration_estimator import sample_generators
 def test_normal_with_constant_rate():
     new_per_day = 1000
     generator = sample_generators.NormalDistributionWithConstantDailyGrowth(0, 1, new_per_day)
-    for days in range(10):
+    for days in range(1, 10):
         sample = generator(days)
-        assert sample.pilot.size == sample.control.size == days * (new_per_day + 1)
+        assert sample.pilot.size == sample.control.size == days * new_per_day
         np.testing.assert_approx_equal(sample.pilot.mean(), 0)
         np.testing.assert_approx_equal(sample.pilot.std(), 1)
         np.testing.assert_approx_equal(sample.pilot.mean(), sample.control.mean())
